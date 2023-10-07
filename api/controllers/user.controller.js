002b9aeec77592dd88ad43cdc816441675bbac30
ali arman <ali@gmail.com>
2023-10-07 04:03:29 -0700

    create user update api

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,3 +1,29 @@
-export const test = (req, res) => {
-  res.send("Hello World");
+import User from "../model/user.model.js";
+import { errorHandler } from "../utils/error.js";
+import bcrypt from "bcryptjs";
+
+export const updateUser = async (req, res, next) => {
+  if (req.user.id !== req.params.id)
+    return next(errorHandler(401, "You can only change you profile"));
+  try {
+    if (req.body.password) {
+      req.body.password = bcrypt.hashSync(req.body.password, 10);
+    }
+    const updateUser = await User.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: {
+          username: req.body.username,
+          email: req.body.email,
+          password: req.body.password,
+          avatar: req.body.avatar,
+        },
+      },
+      { new: true }
+    );
+    const { password: pass, ...rest } = updateUser._doc;
+    res.status(200).json(rest);
+  } catch (error) {
+    next(error);
+  }
 };
